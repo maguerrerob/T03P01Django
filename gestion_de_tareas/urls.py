f04912e7eefa7025749d20dbcb07f1a2fb145cb7
Manuel Alejandro <manuel.guerrero.boza.al@iespoligonosur.org>
2023-10-28 16:56:56 +0200

    fallo 5

--- gestion_de_tareas/urls.py
+++ gestion_de_tareas/urls.py
@@ -4,5 +4,7 @@ from .import views
 urlpatterns = [
     path("", views.vista_gestion_de_tareas, name="vista_gestion_de_tareas"),
     path("proyectos", views.listar_proyectos, name="listar_proyectos"),
-    path("tareas_asociadas_proyectos", views.tareas_proyectos, name="tareas_proyectos")
+    path("tareas_asociadas_proyectos", views.tareas_proyectos, name="tareas_proyectos"),
+    path("usuarios_con_tareas", views.usuarios_asignados_tareas, name="usuarios_asignados_tareas"),
+    path("tareas_con_observaciones", views.tareas_con_observacion, name="tareas_con_observacion")
 ]
--- gestion_de_tareas/views.py
+++ gestion_de_tareas/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Proyecto, Usuario, Tarea
+from .models import Proyecto, Usuario, Tarea, Asignacion_de_Tareas
 
 # Create your views here.
 
@@ -14,3 +14,13 @@ def tareas_proyectos(request):
     QStareas = Tarea.objects.select_related("proyecto").select_related("creador").prefetch_related("usuarios_asignados").prefetch_related("etiquetas_asociadas").all()
     tareas = QStareas.order_by("-fecha_creacion")
     return render(request, "tareas/lista_tareas_asociadas_proyectos.html", {"tareas_asociadas": tareas})
+
+def usuarios_asignados_tareas(request):
+    QSasig = Asignacion_de_Tareas.objects.select_related("usuario")
+    asig = QSasig.order_by("fecha_asignacion")
+    return render(request, "usuarios/usuarios_con_tareas.html", {"asig": asig})
+
+def tareas_con_observacion(request, obs):
+    QStareas = Asignacion_de_Tareas.objects.prefetch_related("tarea")
+    tareas = QStareas.filter(observaciones__contains = obs)
+    return render(request, "tareas/tareas_concretas.html", {"tareas": tareas})
